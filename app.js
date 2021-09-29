f96b55dfdbacdeb29a80ea89e2d79f5d9c4cf406
kartikey sood <kartikeysood@rediffmail.com>
2021-09-29 20:33:38 +0530

    added procfile , port

--- app.js
+++ app.js
@@ -92,8 +92,12 @@ app.post("/compose", (req, res) => {
 
 //server listen
 
-app.listen(3000, (req, res) => {
-  console.log("server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, () => {
+  console.log("started successfully");
 });
 
 //adding read more
